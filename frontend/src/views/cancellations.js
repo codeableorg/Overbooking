dfc0b11b8ce52f31f95f749c6291e809669f0ebc
christopher77 <christoph.roa@gmail.com>
2019-07-17 10:09:16 -0500

    Change reducer for new state (#30)
    * Change reducer for new state
    
    * Show state values in views

--- frontend/src/views/cancellations.js
+++ frontend/src/views/cancellations.js
@@ -5,34 +5,59 @@ import airplane from "../images/airplane.png";
 import { Button } from "../components/ui";
 import ValueCancelation from "../components/value-cancelation";
 import { useRound } from "../selectors";
+import { navigate } from "@reach/router";
+import Header from "../components/header";
+import { useSetCurrentGame } from "../action-hooks";
 
 function FlightDetails() {
   // TODO: WIP animation of values
   const game = useRound();
-  const seatsStatus = game.cancellations - game.overbookingNumber;
-  const totalRevenue = game.totalRevenue / 1000;
-  const [cancellation, setCancellations] = React.useState(game.cancellations);
-  const [overbookingCost, setOverbookingCost] = React.useState(
-    seatsStatus < 0 ? (seatsStatus * game.overbookingCost) / -1000 : 0
+  const {
+    cancellations,
+    overbookingNumber,
+    totalRevenue,
+    overbookingCost,
+    underageCost
+  } = game.games[game.currentGame];
+  const seatsStatus = cancellations - overbookingNumber;
+  const currentTotalRevenue = totalRevenue / 1000;
+  const [cancellation, setCancellations] = React.useState(cancellations);
+  const [currentOverbookingCost, setOverbookingCost] = React.useState(
+    seatsStatus < 0 ? (seatsStatus * overbookingCost) / -1000 : 0
   );
-  const [underageCost, setUnderageCost] = React.useState(
-    seatsStatus > 0 ? (seatsStatus * game.underageCost) / 1000 : 0
+  const [currentUnderageCost, setUnderageCost] = React.useState(
+    seatsStatus > 0 ? (seatsStatus * underageCost) / 1000 : 0
   );
   const [netRevenue, setNetRevenue] = React.useState(
-    totalRevenue - overbookingCost - underageCost
+    currentTotalRevenue - currentOverbookingCost - currentUnderageCost
   );
+
+  const setCurrentGame = useSetCurrentGame();
 
   React.useEffect(() => {}, [cancellation]);
 
+  function changeCurrentGame(event) {
+    event.preventDefault();
+    setCurrentGame(game.currentGame + 1);
+    navigate("/flight-details");
+  }
+
+  function goToScore(event) {
+    setCurrentGame(1);
+    // TODO: we will update the route to the score view
+    navigate("/");
+  }
+
   return (
     <div>
+      <Header />
       <img
         src={airplane}
         css={{ margin: "auto", display: "block", marginBottom: 20 }}
         alt="airplane"
       />
       <div css={{ textAlign: "center", marginBottom: 40 }}>
-        <p>Overbooked seats: {game.overbookingNumber}</p>
+        <p>Overbooked seats: {overbookingNumber}</p>
         <p>Total cancellatioms</p>
         <span css={{ fontSize: 56, fontWeight: "bold" }}>{cancellation}</span>
       </div>
@@ -43,12 +68,19 @@ function FlightDetails() {
           marginBottom: 40
         }}
       >
-        <ValueCancelation value={totalRevenue} label="Total revenue" />
-        <ValueCancelation value={overbookingCost} label="Overbooking cost" />
-        <ValueCancelation value={underageCost} label="Underage cost" />
+        <ValueCancelation value={currentTotalRevenue} label="Total revenue" />
+        <ValueCancelation
+          value={currentOverbookingCost.toFixed(1)}
+          label="Overbooking cost"
+        />
+        <ValueCancelation value={currentUnderageCost} label="Underage cost" />
         <ValueCancelation value={netRevenue.toFixed(1)} label="Net revenue" />
       </div>
-      <Button>Again</Button>
+      {game.currentGame < 7 ? (
+        <Button onClick={changeCurrentGame}>Next Flight</Button>
+      ) : (
+        <Button onClick={goToScore}>My Score</Button>
+      )}
     </div>
   );
 }
--- frontend/src/views/overbooking-number.js
+++ frontend/src/views/overbooking-number.js
@@ -6,10 +6,17 @@ import { useAddOverbookingNumber, useSetTotalRevenue } from "../action-hooks";
 import { Button } from "../components/ui";
 import airplane from "./../images/airplane.png";
 import { navigate } from "@reach/router";
+import Header from "../components/header";
 
 function OverbookingNumber() {
   const [value, setValue] = React.useState(0);
-  const firstRound = useRound();
+  const game = useRound();
+  const {
+    suggestedOverbooking,
+    myCriticalRatio,
+    pricePerSeat,
+    totalSeats
+  } = game.games[game.currentGame];
   const addOverbookingNumber = useAddOverbookingNumber();
   const setTotalRevenue = useSetTotalRevenue();
 
@@ -18,11 +25,9 @@ function OverbookingNumber() {
     const data = event.target.value;
     setValue(data);
   }
-  const seats = firstRound.totalSeats;
-  const pricePerSeat = firstRound.pricePerSeat;
-  const revenue = value * pricePerSeat + seats * pricePerSeat;
+  const revenue = value * pricePerSeat + totalSeats * pricePerSeat;
 
-  const bulletPosition = (value / seats) * 77 + "%";
+  const bulletPosition = (value / totalSeats) * 77 + "%";
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -106,13 +111,14 @@ function OverbookingNumber() {
 
   return (
     <section css={containerCSS}>
+      <Header />
       <img
         src={airplane}
         alt="airplane"
         css={{ marginBottom: 15, height: 180 }}
       />
-      <p>Critical Ratio: {firstRound.myCriticalRatio}</p>
-      <p>Suggested Overbooking: {firstRound.suggestedOverbooking}</p>
+      <p>Critical Ratio: {myCriticalRatio}</p>
+      <p>Suggested Overbooking: {suggestedOverbooking}</p>
       <p css={{ fontWeight: "bolder", marginTop: 25 }}>
         Total Revenue: $ {revenue}
       </p>
@@ -132,7 +138,7 @@ function OverbookingNumber() {
           id="rs-range-line"
           type="range"
           min="0"
-          max={seats}
+          max={totalSeats}
           value={value}
           onChange={handleChange}
           css={sliderCSS}
