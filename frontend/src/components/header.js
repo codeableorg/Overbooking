d3dcebfcaa924b181e1f913aca62f912e144ed0d
christopher <christoph.roa@gmail.com>
2019-07-19 11:55:30 -0500

    Add styles to header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -9,17 +9,58 @@ function Header() {
   return (
     <div
       css={{
+        position: "absolute",
+        top: 8,
+        left: 16,
+        right: 16,
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
-        position: "absolute",
-        top: 0,
-        right: 2,
-        left: 0
+        margin: "10px,10px,10px,10px"
       }}
     >
-      <div>home</div>
-      <div>Flight {game.currentGame}/7</div>
+      <div
+        css={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: 36,
+          height: 36,
+          backgroundColor: "#ffffff",
+          borderRadius: 18
+        }}
+      >
+        <i class="fas fa-home" />
+      </div>
+      <div
+        css={{
+          width: 109,
+          height: 36,
+          borderRadius: 18,
+          backgroundColor: "#ffffff",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <label
+          css={{
+            padding: 8,
+            width: 67,
+            height: 14,
+            fontFamily: "Rubik",
+            fontSize: 14,
+            fontWeight: 500,
+            fontStyle: "normal",
+            fontStretch: "normal",
+            lineHeight: 1,
+            letterSpacing: "normal",
+            color: "#000000"
+          }}
+        >
+          Flight {game.currentGame}/7
+        </label>
+      </div>
     </div>
   );
 }
