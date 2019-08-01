fb18438de922d20850fe6ccb2f4b18039bca7f08
christopher <christoph.roa@gmail.com>
2019-08-01 18:43:40 -0500

    Fix ui styles

--- frontend/src/components/picture-boss.js
+++ frontend/src/components/picture-boss.js
@@ -12,14 +12,15 @@ function PictureBoss({ styles = {} }) {
         width: 100,
         height: 100,
         borderRadius: "50%",
-        overflow: "hidden",
         backgroundColor: "white",
         marginTop: 15,
         ...styles,
         "@media (min-width: 375px)": {
           width: 140,
           height: 140,
-          borderRadius: "50%"
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center"
         }
       }}
     >
