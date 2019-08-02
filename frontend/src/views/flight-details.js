af55f6cc79f6bf5eea93c71457ee7a867d3155ab
Paul Portillo <yummta@gmail.com>
2019-08-02 09:58:01 -0500

    Add images to origin and destination (#85)
    - Add svg to each city
    - Add images to reducer
    - Add number to flight

--- frontend/src/views/flight-details.js
+++ frontend/src/views/flight-details.js
@@ -4,14 +4,14 @@ import React from "react";
 import { Link } from "@reach/router";
 import { useGame, useCurrentGame } from "../selectors";
 import Header from "../components/header";
-import PicturePlane from "./../components/picture-plane";
 import {
   buttonStyles,
   Card,
   TitleView,
   LabelValue,
   Center,
-  ColumnEvenly
+  ColumnEvenly,
+  Row
 } from "../components/ui";
 
 function FlightDetails() {
@@ -20,25 +20,70 @@ function FlightDetails() {
   return (
     <ColumnEvenly>
       <Header show={true} />
-      <PicturePlane />
-      <div css={{ width: "100%" }}>
+      <div>
         <TitleView>
-          <h1>Flight {current}</h1>
+          <h1>Flight {game.flightNumber}</h1>
+          <span
+            css={{
+              textTransform: "uppercase",
+              color: "#596275",
+              fontWeight: 400,
+              fontSize: 12,
+              display: "block",
+              marginTop: 4
+            }}
+          >
+            {game.destination}
+          </span>
         </TitleView>
-        <Card>
-          <LabelValue
-            label="Destination"
-            value={game.destination}
-            border="Bottom"
-          />
-          <LabelValue
-            label="Seat Capacity"
-            value={game.totalSeats}
-            border="Bottom"
-          />
-          <LabelValue label="Date" value={game.dateFlight} border="Bottom" />
-        </Card>
       </div>
+      <section css={{ width: "100%" }}>
+        <div
+          css={{
+            display: "flex",
+            alignItems: "flex-end",
+            width: "100%",
+            position: "relative",
+            bottom: -2
+          }}
+        >
+          <Center>
+            <img
+              src={[game.journey.originImage]}
+              alt={game.journey.originName}
+            />
+          </Center>
+          <Center>
+            <img
+              src={[game.journey.destinationImage]}
+              alt={game.journey.destinationName}
+            />
+          </Center>
+        </div>
+        <div css={{ width: "100%", position: "relative", zIndex: 1 }}>
+          <Card>
+            <Row styles={{ borderBottom: "1px solid #E7EAF1" }}>
+              <LabelValue
+                label="Origin"
+                value={game.journey.originName}
+                border="Right"
+              />
+              <LabelValue
+                label="Destination"
+                value={game.journey.destinationName}
+                border="Right"
+              />
+            </Row>
+            <LabelValue
+              label="Seat Capacity"
+              value={game.totalSeats}
+              border="Bottom"
+            />
+            <LabelValue label="Date" value={game.dateFlight} border="Bottom" />
+          </Card>
+        </div>
+      </section>
+
       <Center>
         <Link to="/critical-ratio" css={buttonStyles}>
           Start Game
